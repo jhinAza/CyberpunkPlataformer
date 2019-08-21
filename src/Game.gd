7a060c71d555ea1f423b1cb660a963415e37402c
Julian Diez <juliand1993@gmail.com>
2019-08-21 21:06:08 +0200

    Add informative labels for GUI

--- src/Game.gd
+++ src/Game.gd
@@ -2,9 +2,21 @@ extends Node2D
 
 var coins: int = 0
 
+func _ready():
+	self._start_game()
+
 func player_detected():
 	print("The player has been detected")
+	self._end_game()
 
 func coin_found():
 	self.coins = self.coins + 1
-	print("Tenemos %s monedas" % self.coins)
+	$GUI/CoinsLabel/Value.text = str(self.coins)
+
+func _end_game():
+	$GUI/PlayerDetected/State.play("Show")
+
+func _start_game():
+	self.coins = 0
+	$GUI/PlayerDetected/State.play("Hidden")
+	
